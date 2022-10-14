e34712f4b1336ec63d411381151645e6e5a64bae
anubhav-sharma-web-apps <anubhav-sharma-web-apps@gmail.com>
2022-10-14 20:10:15 +0530

    Updated UI to show edit view

---
+++ resources/blogs.js
@@ -0,0 +1,98 @@
+const allBlogs = [
+    {
+        "id" : 0,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+    {
+        "id" : 1,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+    {
+        "id" : 2,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+    {
+        "id" : 0,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+    {
+        "id" : 0,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+    {
+        "id" : 0,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+    {
+        "id" : 0,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+    {
+        "id" : 0,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+    {
+        "id" : 0,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+    {
+        "id" : 0,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+    {
+        "id" : 0,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+    {
+        "id" : 0,
+        "title" : "My First Blog",
+        "author" : "Anubhav Sharma",
+        "text" : "This is my first blog",
+        "footnote" : "More will follow",
+        "date" : "13-10-2022"
+    },
+]
